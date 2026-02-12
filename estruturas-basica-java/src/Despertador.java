848d715643b24f0c701f70f7c705f185e385347f
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-02-12 16:53:04 -0300

    Aprendendo usar classe em Java

---
+++ estruturas-basica-java/src/Despertador.java
@@ -0,0 +1,53 @@
+public class Despertador {
+    
+    private int horas;
+    private int minutos;
+    private int soneca;
+
+
+    public Despertador() {
+    }
+
+    public Despertador(int horas, int minutos, int soneca) {
+        this.horas = horas;
+        this.minutos = minutos;
+        this.soneca = soneca;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public int getSoneca() {
+        return soneca;
+    }
+
+    public void setSoneca(int soneca) {
+        this.soneca = soneca;
+    }
+
+    void ativarSoneca() {
+       System.out.println("Mais " + soneca + " minutos");
+    }    
+
+    String agendarAlarme() {
+        return "\nSeu alarme foi configurado as " + horas + ":" + minutos + "h";
+    }
+
+    @Override
+    public String toString() {
+        return "Despertador [horas=" + horas + ", minutos=" + minutos + ", soneca=" + soneca + "]";
+    }
+}
--- estruturas-basica-java/src/metodos.java
+++ estruturas-basica-java/src/metodos.java
@@ -1,17 +1,21 @@
 public class metodos {
 
     public static void main(String[] args) {
-        String mensagemDoAlarme = agendarAlarme(12, 30);
-        System.out.println(mensagemDoAlarme);
-        ativarSoneca();
+        // String mensagemDoAlarme = agendarAlarme(12, 30);
+        // System.out.println(mensagemDoAlarme);
+        // ativarSoneca();
+
+        Despertador despertador = new Despertador(12, 30, 5);
+        System.out.println(despertador.agendarAlarme());
+        despertador.ativarSoneca();
     }
 
-    static void ativarSoneca() {
-       System.out.println("Mais 5 minutos");
-    }
+    // static void ativarSoneca() {
+    //    System.out.println("Mais 5 minutos");
+    // }
     
 
-    static String agendarAlarme(int hora, int minutos) {
-        return "\nSeu alarme foi configurado as " + hora + ":" + minutos + "h";
-    }
+    // static String agendarAlarme(int hora, int minutos) {
+    //     return "\nSeu alarme foi configurado as " + hora + ":" + minutos + "h";
+    // }
 }

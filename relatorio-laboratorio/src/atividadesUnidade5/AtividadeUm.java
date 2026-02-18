4726b35d7cefb9dbb8d89d6246fe6ada85b516c9
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-02-18 09:42:14 -0300

    Refatorando para praticar

--- relatorio-laboratorio/src/atividadesUnidade5/AtividadeUm.java
+++ relatorio-laboratorio/src/atividadesUnidade5/AtividadeUm.java
@@ -1,5 +1,7 @@
 package atividadesUnidade5;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,21 +12,51 @@ public class AtividadeUm {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double valorDaCompra, desconto, valorFinal;
+        List<Double> totalDeCompras = new ArrayList<>();
 
-        System.out.print("Digite o Valor da compra: ");
-        valorDaCompra = sc.nextDouble();
+        double valorDaCompra, valorComDesconto, soma;
+        String resposta;
 
-        if (valorDaCompra >= 500) {
-            desconto = valorDaCompra * 0.80;
-            System.out.printf("Desconto de 20%% aplicado, Valor a pagar: %.2f R$.", desconto);
-        } else if (valorDaCompra >= 200) {
-            desconto = valorDaCompra * 0.90;
-            System.out.printf("Desconto de 10%% aplicado, Valor a pagar: %.2f R$.", desconto);
-        } else {
-            valorFinal = valorDaCompra;
-            System.out.printf("Desconto de 0%% aplicado, Valor a pagar: %.2f R$.", valorFinal);
+        while (true) {
+
+            System.out.print("Vamos fazer uma compra (s/n)? ");
+            resposta = sc.next().trim();
+
+            if (resposta.equalsIgnoreCase("n")) {
+                System.out.println("Obrigado pela visita! Volte sempre.");
+                break;
+            }
+
+            if (!resposta.equalsIgnoreCase("s")) {
+                System.out.println("Resposta inválida. Digite apenas s ou n.");
+                continue;
+            }
+
+            System.out.print("Digite o valor da compra: ");
+            valorDaCompra = sc.nextDouble();
+
+            if (valorDaCompra >= 500) {
+                valorComDesconto = valorDaCompra * 0.80;
+                System.out.printf("Desconto de 20%% aplicado. Valor a pagar: %.2f R$%n", valorComDesconto);
+
+            } else if (valorDaCompra >= 200) {
+                valorComDesconto = valorDaCompra * 0.90;
+                System.out.printf("Desconto de 10%% aplicado. Valor a pagar: %.2f R$%n", valorComDesconto);
+
+            } else {
+                valorComDesconto = valorDaCompra;
+                System.out.printf("Desconto de 0%% aplicado. Valor a pagar: %.2f R$%n", valorComDesconto);
+            }
+
+            totalDeCompras.add(valorComDesconto);
         }
+
+        soma = totalDeCompras.stream()
+                .mapToDouble(Double::doubleValue)
+                .sum();
+
+        System.out.printf("Total gasto nas compras com desconto: %.2f R$%n", soma);
+
         sc.close();
     }
 }

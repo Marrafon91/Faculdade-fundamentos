8c9d57760e1ea22647d01672a824fe7f2bbd0469
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-02-18 09:53:23 -0300

    Atividade 2

---
+++ relatorio-laboratorio/src/atividadesUnidade5/AtividadeDois.java
@@ -0,0 +1,27 @@
+package atividadesUnidade5;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AtividadeDois {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int pulsacao;
+
+        System.out.print("Digite a pulsação: ");
+        pulsacao = sc.nextInt();
+
+        if (pulsacao <= 60) {
+            System.out.print("Bradicardia");
+        } else if (pulsacao <= 100) {
+            System.out.print("Normal");
+        } else {
+            System.out.print("Taquicardia");
+        }
+        sc.close();
+    }
+}

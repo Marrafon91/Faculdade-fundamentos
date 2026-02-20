ea7e5d513f2b91f67d248b90dfdda7cd16ed8f92
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-02-20 06:36:28 -0300

    Uri 1003

--- URI/src/URI/Uri1004.java
+++ URI/src/URI/Uri1004.java
@@ -1,18 +1,16 @@
+package URI;
+
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class Uri1004 {
 
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int SOMA = A + B;
 
-        System.out.println("SOMA = " + SOMA);
 
         sc.close();
     }

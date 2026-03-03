5cf2be8546c560ad4db9ec41ecf0635bb7f258da
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-03-03 06:52:23 -0300

    uri 1018

--- URI/src/URI/Uri1018.java
+++ URI/src/URI/Uri1018.java
@@ -1,7 +1,9 @@
+package URI;
+
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class Uri1018 {
 
     public static void main(String[] args) {
 
@@ -11,7 +13,7 @@ public class Main {
         int valor = sc.nextInt();
         System.out.println(valor);
 
-        int[] notas = {100, 50, 20, 10, 5, 2, 1};
+        int[] notas = {100,50,20,10,5,2,1};
 
         for (int i = 0; i < notas.length; i++) {
             int quantidade = valor / notas[i];

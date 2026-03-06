b88a2d0653f00a2e3c555c51ff906eadeb78e6d6
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-03-06 17:29:14 -0300

    Uri 1040

--- URI/src/URI/Uri1040.java
+++ URI/src/URI/Uri1040.java
@@ -1,23 +1,25 @@
+package URI;
+
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class Uri1040 {
 
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        double n1 = sc.nextDouble();
-        double n2 = sc.nextDouble();
-        double n3 = sc.nextDouble();
-        double n4 = sc.nextDouble();
+        double n1  = sc.nextDouble();
+        double n2  = sc.nextDouble();
+        double n3  = sc.nextDouble();
+        double n4  = sc.nextDouble();
 
         double media = Math.floor((n1 * 2 + n2 * 3 + n3 * 4 + n4 * 1)) / 10;
 
         if (media >= 7.0) {
-            System.out.printf("Media: %.1f%n", media);
-            System.out.println("Aluno aprovado.");
+        System.out.printf("Media: %.1f%n", media);
+        System.out.println("Aluno aprovado.");
         } else if (media < 5.0) {
             System.out.printf("Media: %.1f%n", media);
             System.out.println("Aluno reprovado.");
